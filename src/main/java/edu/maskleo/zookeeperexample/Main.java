92cd75badd5005d39ce16843064aff451f424d6a
maskleo.com <xx@xx.xx>
2018-10-08 14:22:45 +0800

    Initial commit

---
+++ src/main/java/edu/maskleo/zookeeperexample/Main.java
@@ -0,0 +1,31 @@
+package edu.maskleo.zookeeperexample;
+
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
+
+import java.util.concurrent.CountDownLatch;
+
+public class Main implements Watcher {
+
+    private static CountDownLatch countDownLatch = new CountDownLatch(1);
+
+    public static void main(String[] args) throws Exception {
+        ZooKeeper zooKeeper = new ZooKeeper("127.0.0.1:2881", 5000, new Main());
+        System.out.println(zooKeeper.getState());
+        try {
+            countDownLatch.await();
+        }catch (Exception e){
+
+        }
+        System.out.println("ZooKeeper session established.");
+    }
+
+    @Override
+    public void process(WatchedEvent watchedEvent) {
+        System.out.println("Receive watched event:" + watchedEvent);
+        if (Event.KeeperState.SyncConnected == watchedEvent.getState()) {
+            countDownLatch.countDown();
+        }
+    }
+}
---
+++ src/main/java/edu/maskleo/zookeeperexample/ZookeeperExampleApplication.java
@@ -0,0 +1,12 @@
+package edu.maskleo.zookeeperexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZookeeperExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZookeeperExampleApplication.class, args);
+    }
+}
